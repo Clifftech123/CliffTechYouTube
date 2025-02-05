ffd96132800dd1837aee7a60553b6d7277962ef9
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2025-02-05 15:14:01 +0000

    Adding of new video project

---
+++ BlazorCRUD/BlazorCrudApp/Program.cs
@@ -0,0 +1,46 @@
+using BlazorCrudApp.Components;
+using BlazorCrudApp.Data;
+using BlazorCrudApp.Service;
+using Microsoft.EntityFrameworkCore;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection"));
+});
+
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
